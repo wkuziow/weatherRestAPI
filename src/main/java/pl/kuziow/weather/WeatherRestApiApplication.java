cb9d8518136e308f18033d19a566b5ad6d790255
Wojciech Kuziow <wkuziow@gmail.com>
2020-12-16 21:51:12 +0100

    first commit

---
+++ src/main/java/pl/kuziow/weather/WeatherRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kuziow.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kuziow/weather/WeatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kuziow.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
